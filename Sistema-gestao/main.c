8a36f4d486afeb695476fd75db022cb025dff3a3
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-06-05 01:26:27 -0300

    Update relacional

--- Sistema-gestao/main.c
+++ Sistema-gestao/main.c
@@ -1,8 +1,63 @@
+/*Incluindo as bibliotecas para rodar o programa*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int main()
+/*
+Sistema de mercearia utilizando:
+
+stings
+vetores
+struct
+repetiçoes
+matriz
+
+*/
+int saldo = 200;
+
+void impri_cabec(void){
+    printf("***************************************\n");
+    printf("* Bem vindo a mercearia do Ze Ramalho *\n");
+    printf("***************************************\n\n");
+    printf("Saldo do cliente: %d \n",saldo);
+    return;
+}
+
+void padaria(void){
+    printf("==== Menu da padaria ==== \n");
+    printf("Pao frances (unidade) == 0,20 RS \n");
+    printf("Pao doce(unidade) == 0,50 RS \n ");
+    printf("Pao de queijo (unidade) == 1,00 RS \n");
+    printf("Bolo de cenoura (unidade) == 2,30 RS \n");
+    printf("Bolo de chocolate (unidade) == 2,20 RS \n");
+
+    return;
+}
+
+void acougue(void){
+    printf("====Menu da frio==== \n");
+    printf("Queijo de mussarela (100g) == 0,50 RS \n");
+    printf("Queijo mineiro (100g) == 0,70 RS \n");
+    printf("Mortadela (100g) == 0,90 RS \n");
+    printf("Peito de peru (500g) == 2,30 RS \n");
+    printf("Carne muito boa (500g) == 2,20 RS \n");
+
+    return;
+}
+
+int main(int escolha)
 {
-    printf("Hello world!\n");
+    impri_cabec();
+    printf("\n Para ver o menu da padaria digite um(1) para ver o menu do açougue(2)\n");
+    scanf("%d",escolha);
+    if(escolha==1){
+        padaria();
+    }else{
+        acougue();
+    }
+    acougue();
+
     return 0;
 }
