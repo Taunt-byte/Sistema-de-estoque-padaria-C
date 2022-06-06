b658e1e862c7247e7ccdc72425d4022d686d623a
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-06-06 00:00:23 -0300

    Update 4

--- Sistema-gestao/main.c
+++ Sistema-gestao/main.c
@@ -33,6 +33,7 @@ void padaria(void){
     printf("==== Menu da padaria ==== \n");
     printf("Pao frances (unidade) == 0,20 RS \n");
     printf("Pao doce(unidade) == 0,50 RS \n ");
+<<<<<<< Updated upstream
     printf("Pao de queijo (unidade) == 1,00 RS \n");
     printf("Bolo de cenoura (unidade) == 2,30 RS \n");
     printf("Bolo de chocolate (unidade) == 2,20 RS \n");
@@ -42,12 +43,18 @@ void padaria(void){
     printf("Mortadela (100g) == 0,90 RS \n");
     printf("Peito de peru (500g) == 2,30 RS \n");
 =======
+=======
+    printf("Pao de queijo (unidade) == 0,80 RS \n");
+    printf("Bolo de cenoura (unidade) == 2,50 RS \n");
+    printf("Bolo de chocolate (unidade) == 2,50 RS \n");
+>>>>>>> Stashed changes
 
 >>>>>>> 8a36f4d486afeb695476fd75db022cb025dff3a3
     return;
 }
 
 void acougue(void){
+<<<<<<< Updated upstream
     printf("====Menu da frio==== \n");
 <<<<<<< HEAD
 =======
@@ -57,6 +64,14 @@ void acougue(void){
     printf("Peito de peru (500g) == 2,30 RS \n");
 >>>>>>> 8a36f4d486afeb695476fd75db022cb025dff3a3
     printf("Carne muito boa (500g) == 2,20 RS \n");
+=======
+    printf("====Menu de frios ==== \n");
+    printf("Queijo de mussarela (100g) == 0,50 RS \n");
+    printf("Queijo mineiro (100g) == 0,50 RS \n");
+    printf("Mortadela (100g) == 0,80 RS \n");
+    printf("Peito de peru (500g) == 2,50 RS \n");
+    printf("Carne muito boa (500g) == 2,50 RS \n");
+>>>>>>> Stashed changes
 
     return;
 }
@@ -85,6 +100,7 @@ int main(int escolha)
 }
 =======
     printf("\n Para ver o menu da padaria digite um(1) para ver o menu do açougue(2)\n");
+<<<<<<< Updated upstream
     scanf("%d",escolha);
     if(escolha==1){
         padaria();
@@ -93,6 +109,22 @@ int main(int escolha)
     }
     acougue();
 >>>>>>> 8a36f4d486afeb695476fd75db022cb025dff3a3
+=======
+    scanf("%d",&escolha);
+    switch ( escolha ){
+    case 1 :
+        padaria();
+    break;
+>>>>>>> Stashed changes
 
-    return 0;
+    case 2 :
+        acougue();
+    break;
+
+    default :
+    printf ("Valor invalido!\n");
+  }
+
+  getch();
+  return 0;
 }
